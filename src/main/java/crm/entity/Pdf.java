a93a0ca34603bf8b7618d0c7c165ef73946a4e94
Modernize Studio <studio@modernize.com>
2025-11-28 08:26:08 +0000

    Commit for planId: 416

--- src/main/java/crm/entity/Pdf.java
+++ src/main/java/crm/entity/Pdf.java
@@ -16,7 +16,7 @@ import javax.validation.constraints.Size;
 public class Pdf {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
