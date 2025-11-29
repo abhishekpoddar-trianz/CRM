6b26d88a5b7c0d18a7bec7cbeb5f8019329f3706
Modernize Studio <studio@modernize.com>
2025-11-29 12:42:44 +0000

    Commit for planId: 43

--- src/main/java/crm/entity/User.java
+++ src/main/java/crm/entity/User.java
@@ -16,7 +16,7 @@ import jakarta.validation.constraints.NotEmpty;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
